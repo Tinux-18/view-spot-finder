2dab1da836038630e22cdbcfd4171079af3d7b38
Constantin Rigu <rigucatalin@gmail.com>
2022-05-25 16:23:02 +0200

    Working solution

--- find_view_spots.js
+++ find_view_spots.js
@@ -4,44 +4,46 @@ function findViewSpots() {
     const viewSpotNumber = process.argv.slice(2)[1];
     const elements = require(process.argv.slice(2)[0]).elements;
     const values = require(process.argv.slice(2)[0]).values;
-
+    let viewSpots = [];
     // Add values to elements
 
     elements.map((e, i) => {
         e.value = values[i].value;
     });
 
-    console.log("projects :>> ", elements[0]);
+    elements.map(target => {
+        let neighbours = [];
 
-    let viewSpots = [];
-
-    let testElement = elements[0];
-    let testArr = [];
-
-    // Find neighbours
-    for (let i = 0; i < testElement.nodes.length; i++) {
-        for (let j = 0; j < elements.length; j++) {
-            if (
-                elements[j].id !== testElement.id &&
-                elements[j].nodes.includes(testElement.nodes[i])
-            ) {
-                testArr.push(elements[j]);
+        // Find neighbours
+        for (let i = 0; i < target.nodes.length; i++) {
+            for (let j = 0; j < elements.length; j++) {
+                if (
+                    elements[j].id !== target.id &&
+                    elements[j].nodes.includes(target.nodes[i])
+                ) {
+                    neighbours.push(elements[j]);
+                }
             }
         }
-    }
-    let uniqueTestArr = [...new Set(testArr)];
 
-    console.log("testArr :>> ", uniqueTestArr);
-
-    // Find out if target element is a view spot
-    let higherNeighbours = [];
-
-    for (let i = 0; i < uniqueTestArr.length; i++) {
-        if (uniqueTestArr[i].value > testElement.value) {
-            higherNeighbours.push(uniqueTestArr[i].id);
+        // Find out if target element is a view spot
+        if (
+            [...new Set(neighbours)].filter(
+                e => e.value > target.value
+            ).length === 0
+        ) {
+            viewSpots.push(target);
         }
-    }
-    console.log("higherNeighbours :>> ", higherNeighbours);
+    });
+
+    viewSpots.map(viewSpot => {
+        delete viewSpot.nodes;
+    });
+
+    viewSpots.sort((a, b) => {
+        return a.value - b.value;
+    });
+    console.log(viewSpots.reverse().slice(0, viewSpotNumber));
 }
 
 findViewSpots();
