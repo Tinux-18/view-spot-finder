6f7562be87584b5707d1f52242b887db6357e01a
Constantin Rigu <rigucatalin@gmail.com>
2022-06-01 11:54:35 +0200

    Set-up DynamoDB

---
+++ src/functions/view_spots_finder.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const getViewSpots = async event => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message:
+                    "Go Serverless v3.0! Your function executed successfully!",
+                input: event,
+            },
+            null,
+            2
+        ),
+    };
+};
+
+module.exports = {
+    handler: getViewSpots
+}
